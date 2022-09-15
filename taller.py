64ac8b355fe909ddb1236609cff6a08aac7dd3e1
jclugor <jclugor@unal.edu.co>
2022-09-15 09:43:04 -0500

    Se divide en secciones

--- taller.py
+++ taller.py
@@ -1,23 +1,27 @@
-# Trabajo 1/3 DINÁMICA DE ESTRUCTURAS
+'''Trabajo 1/3 DINÁMICA DE ESTRUCTURAS
+#completar luego
+'''
+# %% importación de librerías
 import numpy as np
-# DEFINICIÓN DE VARIABLES
+
+# %% definición de variables
 g = 9.81  # m/s²
-M6 = 2000 # kg/m²
+M6 = 2000 # kg/m² carga losa
 l = 6     # m lado losa
 h = 20    # m altura
 fpc = 210 # kgf/cm² f'c del concreto
 
-# CONDICIONES INICIALES
+# %% condiciones iniciales
 x0 = 0.009*h # m Posición inicial impacto natural, dirección x
-v0 = 8 # m/s Velocidad inicial, dirección x
+v0 = 8       # m/s Velocidad inicial, dirección x
 
-# CÁLCULOS
+# %% cálculos
 A = l*l   # m² Área losa
 m = M6*A  # kg
 delta_max = 0.007*h # m Desplazamiento máximo asumido en dirección x
 E = 15100*np.sqrt(fpc)*g*(100**2) # N/m² módulo de E. del Concreto NSR-10  
 
-# RIGIDEZ DE LA ESTRUCTURA
+# %% cálculo de rigidez
 # Se toma esta rigidez dado que es la que aportan las columnas de forma
 # perpendicular a su eje axial. Se considera una sección cuadrada para las
 # columnas.
@@ -28,5 +32,3 @@ k_serie = 1/(2*(1/k_col))
 k = 2*k_serie # PARALELO
 
 w = np.sqrt(k/m)
-
-# Comentario de Heidy para ver si Git funciona
